159073543d316cf63a031e8e401d673a22da32ad
Axel Böswetter <evilbmp@gmail.com>
2019-05-01 22:22:08 +0200

    [TASK] revert to traditional array syntax in ext_emconf for compatibility reasons

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "migrator".
  ***************************************************************/
-$EM_CONF['migrator'] = [
+$EM_CONF['migrator'] = array(
     'title' => 'DB Migrator',
     'description' => 'TYPO3 DB Migrator',
     'category' => 'be',
@@ -11,9 +11,9 @@ $EM_CONF['migrator'] = [
     'author_email' => 'sebastian@app-zap.de',
     'author_company' => 'app zap',
     'version' => '1.2.1',
-    'constraints' => [
-        'depends' => [
+    'constraints' => array(
+        'depends' => array(
             'typo3' => '6.1.0-9.99.99',
-        ],
-    ],
-];
+        ),
+    ),
+);
