f230bd70d488d468133bcb84cdb1c546e2c17ed8
Axel Böswetter <boeswetter@portrino.de>
2021-05-06 16:21:05 +0200

    [CLEANUP] removes deprecations and updates extension structure for TYPO3 >= v10

--- Configuration/Commands.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'migration:migrateall' => [
-        'class' => \AppZap\Migrator\Command\MigrateAllCommand::class,
-    ],
-];
--- ext_localconf.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'AppZap\\Migrator\\Command\\MigrationCommand';
